a1973a4ea50351f4ec510c25bf80062f2e83c5d7
Hassan Raza <softdeveloper88@gmail.com>
2024-07-10 16:16:32 +0500

    design code updated

---
+++ lib/core/utils/dynamic_link.dart
@@ -0,0 +1,33 @@
+import 'package:firebase_dynamic_links/firebase_dynamic_links.dart';
+import 'package:share_plus/share_plus.dart';
+
+createDynamicLink(postTitle, postUrl, imageUrl) async {
+  final dynamicLinkParams = DynamicLinkParameters(
+    link: Uri.parse(postUrl),
+
+    uriPrefix: "https://doctak.page.link",
+    androidParameters:  AndroidParameters(
+      packageName: "com.kt.doctak",
+      fallbackUrl:Uri.parse(postUrl),
+      minimumVersion: 41,
+    ),
+    iosParameters:  IOSParameters(
+      bundleId: "com.kt.doctak",
+      appStoreId: "6448684340",
+      minimumVersion: "2.0.9",
+      fallbackUrl:Uri.parse(postUrl),
+    ),
+    // googleAnalyticsParameters: const GoogleAnalyticsParameters(
+    //   source: "twitter",
+    //   medium: "social",
+    //   campaign: "example-promo",
+    // ),
+    socialMetaTagParameters: SocialMetaTagParameters(
+      title: postTitle,
+      imageUrl: Uri.parse(imageUrl),
+    ),
+  );
+  final dynamicLink =
+  await FirebaseDynamicLinks.instance.buildShortLink(dynamicLinkParams);
+  Share.share(dynamicLink.shortUrl.toString());
+}
