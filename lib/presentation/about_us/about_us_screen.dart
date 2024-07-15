a1d3cff037ef00e1c95e209eb786d23de73c5561
Hassan Raza <softdeveloper88@gmail.com>
2024-07-15 15:10:28 +0500

    design code updated

---
+++ lib/presentation/about_us/about_us_screen.dart
@@ -0,0 +1,96 @@
+import 'package:doctak_app/presentation/login_screen/login_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:nb_utils/nb_utils.dart';
+
+import '../home_screen/utils/SVCommon.dart';
+
+class AboutUsScreen extends StatefulWidget {
+  const AboutUsScreen({super.key});
+
+  @override
+  State<AboutUsScreen> createState() =>
+      _AboutUsScreenState();
+}
+
+class _AboutUsScreenState extends State<AboutUsScreen> {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: svGetScaffoldColor(),
+        iconTheme: IconThemeData(color: context.iconColor),
+        title: Text('About Us', style: boldTextStyle(size: 20)),
+        elevation: 0,
+        centerTitle: true,
+        actions: const [
+          // IconButton(onPressed: () {}, icon: const Icon(Icons.more_horiz)),
+        ],
+        leading: IconButton(
+          icon:  Icon(Icons.arrow_back_ios_new_rounded,
+              color:svGetBodyColor()),
+          onPressed: () => Navigator.of(context).pop(),
+      ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Expanded(
+              child: SingleChildScrollView(
+                child: Text(
+                  ''''Welcome to Doctak.net!
+Our Mission
+At Doctak.net, our mission is to create a comprehensive and innovative social network for doctors and medical students worldwide. We strive to connect medical professionals, provide valuable resources, and foster collaboration and knowledge-sharing across the healthcare community.
+Who We Are
+Doctak.net is a cutting-edge platform designed specifically for doctors. Our team consists of healthcare professionals, tech enthusiasts, and industry experts who are passionate about revolutionizing the way doctors interact and access information.
+What We Offer
+	•	Job Opportunities: Explore the latest job openings across the Middle East and India, with plans to expand globally.
+	•	Country-Specific Drug Lists: Access comprehensive drug information tailored to each country.
+	•	Professional Networking: Connect with peers, form groups, and discuss medical cases.
+	•	AI-Powered Features: Utilize the latest in artificial intelligence to enhance your medical practice.
+	•	Medical Conferences: Stay updated on conferences worldwide and never miss an important event.
+	•	Medical Guidelines: Access the latest medical guidelines to stay informed on best practices and treatment protocols.
+	•	Ministry of Health Updates and Notifications: Receive timely updates and notifications from ministries of health in your concerned country.
+	•	Continuing Medical Education (CME): Earn CME credits and keep your knowledge up-to-date.
+	•	Differential Diagnosis: Utilize advanced artificial intelligence to aid in accurate differential diagnosis, enhancing diagnostic precision and efficiency.
+How AI Supports Differential Diagnosis
+At Doctak.net, we leverage advanced artificial intelligence technologies to support doctors in making accurate and timely differential diagnoses. Our AI algorithms analyze the given patient data, symptoms, and medical histories to generate insights and suggest potential diagnoses. This assists healthcare professionals in making informed decisions and improving patient care outcomes.
+Case Discussion
+Engage in in-depth discussions on medical cases with your peers, leveraging AI insights to explore differential diagnoses and treatment strategies collaboratively.
+Follow Us
+Stay connected and follow us on social media:
+	•	Facebook: [Insert Facebook page link] 
+	•	LinkedIn: [Insert LinkedIn page link]
+Contact Us
+Have questions or feedback? Contact us:
+	•	Email: [our email address]
+	•	WhatsApp : [Link]
+	•	Address: [Insert physical address, if applicable
+
+Our Vision
+We envision a world where doctors are seamlessly connected, empowered with the latest technology, and have access to a wealth of resources that support their professional growth and improve patient care.
+Our Values
+	•	Innovation: We embrace the latest technological advancements to provide cutting-edge solutions for the medical community.
+	•	Collaboration: We believe in the power of collaboration and strive to create a platform where doctors can share knowledge and support each other.
+	•	Excellence: We are committed to providing the highest quality resources and services to our users.
+	•	Integrity: We maintain the highest standards of integrity and ethics in everything we do.
+
+
+Join Us
+Be a part of the Doctak.net community and experience a new era of medical networking and resources. Together, we can advance the field of medicine and improve healthcare outcomes worldwide.
+Thank you for choosing Doctak.net!
+              ''',
+                  style: GoogleFonts.poppins(fontSize: 16.0,),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/presentation/terms_and_condition_screen/terms_and_condition_screen.dart
@@ -0,0 +1,143 @@
+import 'package:doctak_app/presentation/login_screen/login_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:nb_utils/nb_utils.dart';
+
+class TermsAndConditionScreen extends StatefulWidget {
+  const TermsAndConditionScreen({super.key});
+
+  @override
+  State<TermsAndConditionScreen> createState() =>
+      _TermsAndConditionScreenState();
+}
+
+class _TermsAndConditionScreenState extends State<TermsAndConditionScreen> {
+  bool _isChecked = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Terms and Conditions'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+             Expanded(
+              child: SingleChildScrollView(
+                child: Text(
+                  ''''
+                terms of Service for DocTak.net
+
+Last Updated: July 2024
+
+Welcome to DocTak.net(“Terms”) govern your use of the App/web, which is developed and provided by “DocTak App developers”. By accessing or using the App, you agree to be bound by these Terms. If you do not agree to these Terms, do not use the App.
+
+ 1. Acceptance of Terms
+By creating an account or otherwise using the App/web, you confirm that you have read, understood, and agree to these Terms and our Privacy Policy.
+
+ 2. Eligibility
+The App/web is intended for use by licensed medical professionals, including doctors and other healthcare providers. By using the App/web, you represent and warrant that you are a licensed medical professional and have the right, authority, and capacity to enter into these Terms.
+
+ 3. User-Generated Content (UGC)
+a. *Content Creation*: The App/web allows users to create, upload, and share content including profiles, posts, comments, case discussions, and more (“User-Generated Content” or “UGC”). 
+
+b. *Responsibility for UGC*: You are solely responsible for the UGC you create and share. You agree not to post content that is defamatory, harmful, illegal, or otherwise objectionable.
+
+c. *License to UGC*: By posting UGC, you grant us a non-exclusive, worldwide, royalty-free, perpetual license to use, modify, reproduce, distribute, and display such content in connection with the operation of the App/web.
+
+d. *Prohibited Content and Behaviors*: You agree not to post content that:
+   - Violates any applicable laws or regulations
+   - Contains hate speech, harassment, or threats
+   - Is obscene, pornographic, or offensive
+   - Infringes on intellectual property rights
+   - Contains false or misleading information
+   - Promotes illegal activities
+
+4. Terms of Use
+a. *Account Security*: You are responsible for maintaining the confidentiality of your account credentials and for all activities that occur under your account.
+
+b. *Prohibited Activities*: You agree not to:
+   - Use the App/web for any unlawful purpose
+   - Attempt to gain unauthorized access to the App/web or other accounts
+   - Engage in any activity that could harm or disrupt the App/web or its users
+
+c. *Reporting Violations*: If you encounter any content or activity that violates these Terms, please report it to us using the reporting mechanisms provided within the App/web or by contacting info@DocTak.net.
+
+ 5. Jobs, Conferences, CME, and MOH Updates
+a. *Job Listings*: The App/web may provide information on job opportunities for medical professionals. We are not responsible for the accuracy or legitimacy of job listings.
+
+b. *Conferences and Events*: The App may feature information about medical conferences and events. We do not endorse or guarantee the quality or availability of these events.
+
+c. *CME and MOH Updates*: The App/web may provide updates on Continuing Medical Education (CME) opportunities and Ministry of Health (MOH) updates. While we strive to provide accurate information, we do not guarantee the completeness or reliability of such updates.
+
+ 6. Case Discussions
+The App/web allows for the discussion of medical cases among users. You agree to respect patient confidentiality and privacy at all times and to follow all applicable laws and regulations regarding patient information.
+
+7. Termination
+We reserve the right to suspend or terminate your account and access to the App/web if you violate these Terms or engage in conduct that we determine to be inappropriate or harmful.
+
+8. Disclaimer of Warranties
+The App/web is provided on an “as is” and “as available” basis. We do not warrant that the App/web will be uninterrupted, error-free, or free of harmful components. We disclaim all warranties, express or implied, including but not limited to the warranties of merchantability and fitness for a particular purpose.
+
+9. Limitation of Liability
+To the fullest extent permitted by law, we shall not be liable for any indirect, incidental, special, consequential, or punitive damages, or any loss of profits or revenues, whether incurred directly or indirectly, or any loss of data, use, goodwill, or other intangible losses resulting from your use of the App/web.
+
+ 10. Indemnification
+You agree to indemnify and hold us harmless from any claims, damages, liabilities, and expenses (including reasonable attorneys’ fees) arising out of or related to your use of the App/web, your violation of these Terms, or your violation of any rights of another.
+
+
+ 11. Changes to Terms
+We may modify these Terms at any time. We may notify you of any changes by posting the new Terms within the App./web Your continued use of the App after the changes become effective constitutes your acceptance of the revised Terms.
+
+12. Governing Law
+These Terms shall be governed by and construed in accordance with the laws of [Your Country/State], without regard to its conflict of law principles.
+
+13. Contact Us
+If you have any questions about these Terms, please contact us at info@DocTak.net.
+
+By using the App, you acknowledge that you have read, understood, and agree to be bound by these Terms of Service''',
+                    style: GoogleFonts.poppins(fontSize: 16.0,),
+                ),
+              ),
+            ),
+            Row(
+              children: <Widget>[
+                Checkbox(
+                  value: _isChecked,
+                  onChanged: (bool? value) {
+                    setState(() {
+                      _isChecked = value!;
+                    });
+                  },
+                ),
+                const Expanded(
+                  child: Text(
+                    'I agree to the Terms and Conditions',
+                    style: TextStyle(fontSize: 16.0),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 20.0),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: _isChecked
+                    ? () async {
+                  SharedPreferences prefs = await SharedPreferences.getInstance();
+                  await prefs.setBool('acceptTerms', true);
+                  LoginScreen().launch(context, isNewTask: true);
+                      }
+                    : null,
+                child: const Text('Next'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
