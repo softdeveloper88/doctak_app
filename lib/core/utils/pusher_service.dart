1a458f62ac0c1cc29f11d409f37a26c7b5fcd097
Hassan Raza <softdeveloper88@gmail.com>
2024-07-23 11:47:18 +0500

    design code updated

---
+++ lib/core/utils/pusher_service.dart
@@ -0,0 +1,44 @@
+import 'package:doctak_app/core/utils/app/AppData.dart';
+import 'package:pusher_client/pusher_client.dart';
+class PusherService {
+  late PusherClient pusher;
+  late Channel channel;
+
+  PusherService(String userId) {
+    PusherOptions options = PusherOptions(
+      cluster: 'ap2',
+      encrypted: true,
+    );
+
+    pusher = PusherClient(
+      '8c57d1a09617aace9be6',
+      options,
+      autoConnect: false,
+    );
+    pusher.onConnectionStateChange((state) {
+      print("previousState: ${state!.previousState}, currentState: ${state.currentState}");
+    });
+
+    pusher.onConnectionError((error) {
+      print("error: ${error!.message}");
+    });
+
+    connect(userId);
+  }
+
+  void connect(String userId) {
+    pusher.connect();
+    channel = pusher.subscribe('user.$userId');
+
+    // Bind to the event name, not the fully qualified class name
+    channel.bind('NotificationEvent', (PusherEvent? event) {
+      print(event!.data);
+      // Handle the notification here
+    });
+  }
+
+  void disconnect() {
+    pusher.unsubscribe('user.${AppData.logInUserId}');
+    pusher.disconnect();
+  }
+}
