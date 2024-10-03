53f610accc0456b1f188a64802d704c43f8c9f18
Hassan Raza <softdeveloper88@gmail.com>
2024-10-03 14:45:53 +0500

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	lib/presentation/home_screen/home/components/SVPostComponent.dart

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,9 @@
 import UIKit
 import Flutter
+import Firebase
+import FirebaseMessaging
+import UserNotifications
+import BackgroundTasks
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,7 +12,41 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
-
+      let pushManager = PushNotificationReceiver()
+              pushManager.registerForPushNotifications()
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+    @objc func methodOfReceivedNotification(notification: Notification) {
+             print("Value of notification : ", notification.object ?? "")
+          
+      
+     }
+     override func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+       print(deviceToken)
+         Messaging.messaging().apnsToken = deviceToken //*THIS WAS MISSING*
+         super.application(application, didRegisterForRemoteNotificationsWithDeviceToken: deviceToken) //Not sure if calling super is required, but did anyway
+       }
+     @available(iOS 10.0, *)
+     public override func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+         print(response.notification.request.content.userInfo)
+         print("Allah")
+    
+         
+     }
+   
+     @available(iOS 10.0, *)
+     public override func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+         print(notification.request.content.userInfo)
+        
+     }
+     
+       
+
+     override func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Swift.Void) {
+         
+//         NotificationCenter.default.addObserver(self, selector: #selector(reinstateBackgroundTask),
+//                          name: UIApplication.didBecomeActiveNotification, object: nil)
+//
+         
+     }
 }
---
+++ ios/Runner/PushNotificationReceiver.swift
@@ -0,0 +1,98 @@
+import Foundation
+import Firebase
+import FirebaseMessaging
+import UIKit
+import UserNotifications
+import AVFoundation
+class PushNotificationReceiver: NSObject,UIApplicationDelegate,UNUserNotificationCenterDelegate{
+    var window: UIWindow?
+     let gcmMessageIDKey = "com.doctak.ios"
+    func registerForPushNotifications() {
+//        print("without Swift code excute")
+        if #available(iOS 10.0, *) {
+            // For iOS 10 display notification (sent via APNS)
+            UNUserNotificationCenter.current().delegate = self
+            let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+            UNUserNotificationCenter.current().requestAuthorization(
+                options: authOptions,
+                completionHandler: {_, _ in })
+            // For iOS 10 data message (sent via FCM)
+           
+
+        } else {
+           
+            let settings: UIUserNotificationSettings =
+                UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+            UIApplication.shared.registerUserNotificationSettings(settings)
+        }
+        UIApplication.shared.registerForRemoteNotifications()
+    
+    }
+    func application(_ application: UIApplication,
+                       didReceiveRemoteNotification userInfo: [AnyHashable: Any]) {
+        // If you are receiving a notification message while your app is in the background,
+        // this callback will not be fired till the user taps on the notification launching the application.
+        // TODO: Handle data of notification
+        // With swizzling disabled you must let Messaging know about the message, for Analytics
+        // Messaging.messaging().appDidReceiveMessage(userInfo)
+        // Print message ID.
+        if let messageID = userInfo[gcmMessageIDKey] {
+          print("Message ID: \(messageID)")
+        }
+      
+        // Print full message.
+        print(userInfo)
+      }
+
+      // [START receive_message]
+      func application(_ application: UIApplication,
+                       didReceiveRemoteNotification userInfo: [AnyHashable: Any],
+                       fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult)
+                         -> Void) {
+        // If you are receiving a notification message while your app is in the background,
+        // this callback will not be fired till the user taps on the notification launching the application.
+        // TODO: Handle data of notification
+        // With swizzling disabled you must let Messaging know about the message, for Analytics
+        // Messaging.messaging().appDidReceiveMessage(userInfo)
+        // Print message ID.
+        if let messageID = userInfo[gcmMessageIDKey] {
+          print("Message ID: \(messageID)")
+        }
+         
+        // Print full message.
+        print(userInfo)
+
+        completionHandler(UIBackgroundFetchResult.newData)
+      }
+
+      // [END receive_message]
+      func application(_ application: UIApplication,
+                       didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        print("Unable to register for remote notifications: \(error.localizedDescription)")
+      }
+    
+      // This function is added here only for debugging purposes, and can be removed if swizzling is enabled.
+      // If swizzling is disabled then this function must be implemented so that the APNs token can be paired to
+      // the FCM registration token.
+      func application(_ application: UIApplication,
+                       didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        print("APNs token retrieved: \(deviceToken)")
+
+        // With swizzling disabled you must set the APNs token here.
+        // Messaging.messaging().apnsToken = deviceToken
+      }
+    
+    
+    @available(iOS 10.0, *)
+    public  func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        print(notification.request.content.userInfo)
+        
+      
+    }
+
+     func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        
+        print(response.notification.request.content.categoryIdentifier)
+    }
+   
+    }
